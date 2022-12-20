eecf9b52da6ecadef744ebd26668925c0b671582
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-12-20 12:54:16 +0100

    Introduce Parameter currentPlayer to method setPlayerToStartField()

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -125,15 +125,15 @@ public class Game {
     public void add(String playerName) {
         Player player = new Player(playerName);
         players.add(player);
-        setPlayerToStartField();
+        setPlayerToStartField(players.size());
         purses[players.size()] = 0;
         penaltyBox.movePlayerToPenaltyBox(players.size(), false);
         System.out.println(player + " was added");
         System.out.println("They are player number " + players.size());
     }
 
-    private void setPlayerToStartField() {
-        places[players.size()] = 0;
+    private void setPlayerToStartField(int currentPlayer) {
+        places[currentPlayer] = 0;
     }
 
     public void roll(int rolledNumber) {
