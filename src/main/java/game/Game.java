11d808e1d0000cbe76def7c175522722c06cc8e2
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-12-20 12:49:39 +0100

    Introduce Parameter currentPlayer to method movePLayer()

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -139,7 +139,7 @@ public class Game {
             if (isOdd(rolledNumber)) {
                 players.get(currentPlayer).setAllowedToAnswer(true);
                 System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
-                movePlayer(rolledNumber);
+                movePlayer(rolledNumber, currentPlayer);
                 System.out.println(players.get(currentPlayer)
                         + "'s new location is "
                         + places[currentPlayer]);
@@ -150,7 +150,7 @@ public class Game {
                 players.get(currentPlayer).setAllowedToAnswer(false);
             }
         } else {
-            movePlayer(rolledNumber);
+            movePlayer(rolledNumber, currentPlayer);
             System.out.println(players.get(currentPlayer)
                     + "'s new location is "
                     + places[currentPlayer]);
@@ -163,7 +163,7 @@ public class Game {
         return rolledNumber % 2 != 0;
     }
 
-    private void movePlayer(int rolledNumber) {
+    private void movePlayer(int rolledNumber, int currentPlayer) {
         int gameFieldToMove = places[currentPlayer] + rolledNumber;
         if (gameFieldToMove > 11) {
             gameFieldToMove -= NUMBER_OF_GAME_FIELDS;
