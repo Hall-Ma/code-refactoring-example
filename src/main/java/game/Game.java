c99a4cdb77b8d4f58c4f80b999e28263e0eeb939
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-12-20 13:18:54 +0100

    Introduce parameter to method didPlayerWin()

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -211,7 +211,7 @@ public class Game {
             if (players.get(currentPlayer).isAllowedToAnswer()) {
                 System.out.println("Answer was correct!!!!");
                 addCoinsToPlayer(currentPlayer, players.get(currentPlayer));
-                boolean winner = didPlayerWin();
+                boolean winner = didPlayerWin(currentPlayer);
                 selectNextPlayerInTurn();
                 return winner;
             } else {
@@ -221,7 +221,7 @@ public class Game {
         } else {
             System.out.println("Answer was corrent!!!!");
             addCoinsToPlayer(currentPlayer, players.get(currentPlayer));
-            boolean winner = didPlayerWin();
+            boolean winner = didPlayerWin(currentPlayer);
             selectNextPlayerInTurn();
             return winner;
         }
@@ -248,7 +248,7 @@ public class Game {
         if (currentPlayer == players.size()) currentPlayer = 0;
     }
 
-    private boolean didPlayerWin() {
+    private boolean didPlayerWin(int currentPlayer) {
         return !(purses[currentPlayer] == COINS_NEEDED_TO_WIN);
     }
 
