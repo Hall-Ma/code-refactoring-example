7ea355ebb27f482bda0422f52a286f311361f38e
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-12-20 12:53:03 +0100

    Extract method setPlayerToStartField() out of method add()

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -128,12 +128,16 @@ public class Game {
         for (String playerName : playerNames) {
             Player player = new Player(playerName);
             players.add(player);
-            places[players.size()] = 0;
+            setPlayerToStartField();
             purses[players.size()] = 0;
             penaltyBox.movePlayerToPenaltyBox(players.size(), false);
             System.out.println(player + " was added");
             System.out.println("They are player number " + players.size());
         }
+    }
+
+    private void setPlayerToStartField() {
+        places[players.size()] = 0;
     }
 
     public void roll(int rolledNumber) {
